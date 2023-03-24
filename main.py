307342c28ae24133e31b68f3e6d8bfcbd12f59a5
sujithkumar0017 <sujith.tesark@gmail.com>
2023-03-24 19:54:06 +0530

    db schema

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from typing import Optional, List
-from fastapi import FastAPI, Path, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
+from api import *
+from db.db_setup import engine
+from db.models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(
@@ -21,31 +21,6 @@ app = FastAPI(
         "name": "MIT",
     },
 )
-users: List = []
-tags_metadata = [
-    {"name": "Get Users", "description": "The Operation will get all the users data"},
-    {"name": "Post Users", "description": "The Operation is to create the user"},
-    {
-        "name": "Get User By Id",
-        "description": "This Operation is used the retrieve the Particular User Details",
-    },
-]
-
-
-@app.get("/users", response_model=List[User], tags=["Get Users"])
-async def get_users():
-    return users
-
-
-@app.post("/users", tags=["Post Users"])
-async def post_users(user: User):
-    users.append(user)
-    return "success"
-
-
-@app.get("/users/{id}", tags=["Get User By Id"])
-async def get_user_by_id(
-    id: int = Path(..., description="The ID of the user you want to retrieve.", gt=2),
-    q: str = Query(None, max_length=5),
-):  # gt=2 --> greater than or equals to 2
-    return {"user": users[id], "query": q}
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
